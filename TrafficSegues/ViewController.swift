ee229392112390ba3c2d670a5c6a2e8c3d08707f
JohnPGreen <johnpgreen60@gmail.com>
2018-11-13 03:59:47 -0500

    part 3

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -12,13 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
-        
-    }
-    
-    
+
 }
 
